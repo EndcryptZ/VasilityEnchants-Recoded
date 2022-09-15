fa3e2cab689c3545bbeb4293274e3b6def4925bd
ExEndV2 <pternej32@gmail.com>
2022-09-15 20:40:48 +0800

    Fixed Reload not updating the "AnvilOpenGUI"

--- src/main/java/com/me/exendv2/vasilityenchantsrecoded/listeners/anvilListener.java
+++ src/main/java/com/me/exendv2/vasilityenchantsrecoded/listeners/anvilListener.java
@@ -1,6 +1,6 @@
 package com.me.exendv2.vasilityenchantsrecoded.listeners;
 
-import com.me.exendv2.vasilityenchantsrecoded.utils.ConfigManager;
+import com.me.exendv2.vasilityenchantsrecoded.VasilityEnchants;
 import com.me.exendv2.vasilityenchantsrecoded.utils.GUIManager;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
@@ -9,15 +9,12 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class anvilListener implements Listener {
-
-    ConfigManager configManager = new ConfigManager();
     GUIManager guiManager = new GUIManager();
 
     @EventHandler
     public void onAnvilClick(PlayerInteractEvent e){
 
-        boolean anvilEnabled = configManager.isAnvilEnabled();
-        if (!(anvilEnabled)) {return;}
+        if (!(VasilityEnchants.instance.getConfig().getBoolean("AnvilOpenGUI"))) {return;}
         if (e.getClickedBlock() == null) {return;}
         if (!e.getClickedBlock().getType().equals(Material.ANVIL)) {return;}
         if (!(e.getAction() == Action.RIGHT_CLICK_BLOCK)) {return;}
